23cf179907e51bf4faf24545e4fc2197351013ed
Madusha Lakshan <dmmadushalw1999@gmail.com>
2024-09-26 00:16:22 +0530

    Initial

---
+++ Hotel_Management/src/main/java/com/customer/LoginServlet.java
@@ -0,0 +1,40 @@
+package com.customer;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+
+
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String username = request.getParameter("uid");
+		String password = request.getParameter("pass");
+		
+		try {
+
+			List<Customer> cusDetails = CustomerDBUtil.validate(username, password);
+			request.setAttribute("cusDetails", cusDetails);
+			
+		}catch(Exception e2) {
+			System.err.println(e2);
+		}
+		
+		
+		RequestDispatcher dis = request.getRequestDispatcher("useraccount.jsp");
+		dis.forward(request, response);
+		
+		
+		
+		
+	}
+
+}
